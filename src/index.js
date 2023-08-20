923b0aeb52fe4f26d58625431832e5113d3f2e3e
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2023-08-20 21:54:13 +0700

    +scss npm install node-sass

--- src/index.js
+++ src/index.js
@@ -3,14 +3,24 @@ const morgan = require('morgan')
 const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
+const path = require('path')
+
+// app.use(express.static('./src/public'))
+app.use(express.static(path.join(__dirname, '/public')))
+
 
 app.use(morgan('combined'))
-app.engine('handlebars', handlebars.engine())
-app.set('view engine', 'handlebars')
+app.engine('hbs', handlebars.engine({extname: '.hbs'}))
+app.set('view engine', 'hbs')
 app.set('views', './src/resources/views')
+
 
 app.get('/', (req, res) => {
   res.render('home')
+})
+
+app.get('/news', (req, res) => {
+  res.render('news')
 })
 
 app.listen(port, () => {
