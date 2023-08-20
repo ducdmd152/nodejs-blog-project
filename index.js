691e68a4cf5f85b17cb9f1bd9c965bfe033aeea1
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2023-08-20 20:06:14 +0700

    +morgan

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
+app.use(morgan('combined'))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(`
+    <h1>Hellow world</h1>
+  `)
 })
 
 app.listen(port, () => {
