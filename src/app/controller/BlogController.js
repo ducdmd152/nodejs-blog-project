fbd92297659553e94e6afdd9fc31bdbceb6ddd6f
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2023-08-21 19:36:30 +0700

    Completed Func Handler

--- src/app/controller/BlogController.js
+++ src/app/controller/BlogController.js
@@ -3,8 +3,10 @@ const { mutipleMongooseToObject } = require("../../util/mongoose");
 
 class BlogController {
   // [GET] /blog/:slug
-  show(req, res) {
-    res.render("search");
+  show(req, res, next) {
+    Blog.findOne({ slug: req.params.slug })
+      .then((blog) => res.json(blog))
+      .catch(next);
   }
 }
 
