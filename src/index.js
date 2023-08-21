6c8b61d77dd90c0d56e8ce5223dcf2cdf9988fdc
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2023-08-21 13:19:10 +0700

    added prettier

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
-const express = require("express");
-const morgan = require("morgan");
-const handlebars = require("express-handlebars");
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
-const path = require("path");
+const path = require('path');
 
-const route = require("./routes/index.route");
+const route = require('./routes/index.route');
 
 // app.use(express.static('./src/public'))
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, '/public')));
 
 // add middleware to pre-handle param to body
 app.use(
   express.urlencoded({
     extended: true,
-  })
+  }),
 );
 app.use(express.json());
 
-app.use(morgan("combined"));
-app.engine("hbs", handlebars.engine({ extname: ".hbs" }));
-app.set("view engine", "hbs");
-app.set("views", "./src/resources/views");
+app.use(morgan('combined'));
+app.engine('hbs', handlebars.engine({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', './src/resources/views');
 
 // Routes init
 route(app);
--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,9 +1,9 @@
-const newsRouter = require("./news.route.js");
-const siteRouter = require("./site.route.js");
+const newsRouter = require('./news.route.js');
+const siteRouter = require('./site.route.js');
 
 const route = (app) => {
-  app.use("/news", newsRouter);
-  app.use("/", siteRouter);
+  app.use('/news', newsRouter);
+  app.use('/', siteRouter);
 };
 
 module.exports = route;
