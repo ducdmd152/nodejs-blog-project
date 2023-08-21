504003ec2d1c33be1b0423f6b6a52904c4703d3b
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2023-08-21 20:32:35 +0700

    Done BLOG CREATE

--- src/app/controller/BlogController.js
+++ src/app/controller/BlogController.js
@@ -24,6 +24,40 @@ class BlogController {
       )
       .catch(next);
   }
+
+  // [GET] /blog/create
+  create(req, res, next) {
+    res.render("blogs/create");
+  }
+
+  // [POST] /blog/store
+  store(req, res, next) {
+    Blog.create({
+      title: req.body.title,
+      desc: req.body.desc,
+      image: req.body.image,
+      slug: req.body.title.toLowerCase().replace(/\s+/g, "-"),
+      date: new Date(),
+    })
+      .then((blog) =>
+        res.render("blogs/show", {
+          blog: {
+            ...singleMongooseToObject(blog),
+            postedTime: singleMongooseToObject(blog)
+              .date.toLocaleString("en-US", {
+                weekday: "short",
+                month: "short",
+                day: "numeric",
+                year: "numeric",
+                hour: "numeric",
+                minute: "numeric",
+              })
+              .replace(/,/g, ""),
+          },
+        })
+      )
+      .catch(next);
+  }
 }
 
 module.exports = new BlogController();
